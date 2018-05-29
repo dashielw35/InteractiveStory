0dd287c64734115e8265c4f5605d6d78cd2b4767
dweidman0384 <dashweidman@gmail.com>
2018-05-29 15:35:45 -0700

    Initial commit -- New Repository

---
+++ app/src/main/java/com/example/dweidman0384/interactivestory/model/Choice.java
@@ -0,0 +1,31 @@
+package com.example.dweidman0384.interactivestory.model;
+
+/**
+ * Created by dweidman0384 on 5/25/2018.
+ */
+
+public class Choice {
+    private int textId;
+    private int nextPage;
+
+    public Choice(int textId, int nextPage) {
+        this.textId = textId;
+        this.nextPage = nextPage;
+    }
+
+    public int getTextId() {
+        return textId;
+    }
+
+    public void setTextId(int textId) {
+        this.textId = textId;
+    }
+
+    public int getNextPage() {
+        return nextPage;
+    }
+
+    public void setNextPage(int nextPage) {
+        this.nextPage = nextPage;
+    }
+}
