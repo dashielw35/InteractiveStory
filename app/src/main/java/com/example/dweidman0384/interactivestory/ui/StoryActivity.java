e0df01f3645f8c34b442546f0487166c379b8770
dweidman0384 <dashweidman@gmail.com>
2018-05-31 15:13:57 -0700

    dash

--- app/src/main/java/com/example/dweidman0384/interactivestory/ui/StoryActivity.java
+++ app/src/main/java/com/example/dweidman0384/interactivestory/ui/StoryActivity.java
@@ -6,6 +6,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -14,15 +15,21 @@ import com.example.dweidman0384.interactivestory.R;
 import com.example.dweidman0384.interactivestory.model.Page;
 import com.example.dweidman0384.interactivestory.model.Story;
 
+import java.util.Stack;
+
+import static android.R.attr.name;
+
 public class StoryActivity extends AppCompatActivity {
 
     public static final String TAG = StoryActivity.class.getSimpleName();
 
+    private String name;
     private Story story;
     private ImageView storyImageView;
     private TextView storyTextView;
     private Button choice1Button;
     private Button choice2Button;
+    private Stack<Integer> pageStack = new Stack<Integer>();
 
 
 
@@ -37,7 +44,7 @@ public class StoryActivity extends AppCompatActivity {
         choice2Button = (Button)findViewById(R.id.choice2Button);
 
         Intent intent = getIntent();
-        String name = intent.getStringExtra(getString(R.string.key_name));
+        name = intent.getStringExtra(getString(R.string.key_name));
         if (name == null || name.isEmpty()) {
             name = "Friend";
         }
@@ -47,10 +54,68 @@ public class StoryActivity extends AppCompatActivity {
         loadPage(0);
     }
     private void loadPage(int pageNumber) {
-        Page page = story.getPage(pageNumber);
+        pageStack.push(pageNumber);
+        final Page page = story.getPage(pageNumber);
 
         Drawable image = ContextCompat.getDrawable(this, page.getImageId());
         storyImageView.setImageDrawable(image);
+
+
+        String pageText = getString(page.getTextId());
+        // add name if placeholder included
+        pageText = String.format(pageText, name);
+        storyTextView.setText(pageText);
+
+        if (page.isFinalPage()){
+            choice1Button.setVisibility(View.INVISIBLE);
+            choice2Button.setText(R.string.play_again_button_text);
+            choice2Button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //finish();
+                    loadPage(0);
+                }
+            });
+
+        }
+        else {
+
+            loadButtons(page);
+        }
+
+    }
+
+    private void loadButtons(final Page page) {
+        choice1Button.setVisibility(View.VISIBLE);
+        choice1Button.setText(page.getChoice1().getTextId());
+        choice1Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int nextPage = page.getChoice1().getNextPage();
+                loadPage(nextPage);
+            }
+        });
+
+        choice2Button.setVisibility(View.VISIBLE);
+        choice2Button.setText(page.getChoice2().getTextId());
+        choice2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int nextPage = page.getChoice2().getNextPage();
+                loadPage(nextPage);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        pageStack.pop();
+        if (pageStack.isEmpty()) {
+            super.onBackPressed();
+        }
+        else {
+            loadPage(pageStack.pop());
+        }
 
     }
 }
